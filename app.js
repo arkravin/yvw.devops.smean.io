034f8344a261ff5e0ec75b82318c1ff1fecd26b7
arkravin <42764675+arkravin@users.noreply.github.com>
2019-03-12 13:00:36 +1100

    udpate description

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req, res)=> res.send('hello world'));
+app.get('/', (req, res)=> res.send('hello world from GIT'));
 
 app.listen (3000, ()=>{
 
